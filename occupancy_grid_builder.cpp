b2eea0013d3fc0bcc52cb796c206458a94a7bfcd
AzulBarr <85140537+AzulBarr@users.noreply.github.com>
2026-01-25 15:52:51 -0300

    Add comments to main function in occupancy_grid_builder
    Added comments to clarify the purpose of rclcpp functions.

--- occupancy_grid_builder.cpp
+++ occupancy_grid_builder.cpp
@@ -104,9 +104,9 @@ private:
 };
 
 int main(int argc, char **argv) {
-    rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv); // ROS Client Library
     auto node = std::make_shared<OccupancyGridBuilder>();
-    rclcpp::spin(node);
+    rclcpp::spin(node); // Entrar en el loop de eventos de ROS y no salir hasta que el nodo termine
     rclcpp::shutdown();
     return 0;
 }
